500a8fa4657353a1245ee80ba1d72f85d0cea9c5
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-02-07 17:51:53 +0200

    create select area

--- app.js
+++ app.js
@@ -41,7 +41,13 @@ function deleteCheck(e) {
     // delete todo item
     if (item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
-        todo.remove();
+         //animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
+       
+        
     }
 // check mark
 if (item.classList[0] === "complete-btn") {
